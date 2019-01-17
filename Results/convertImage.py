6cdbfeba5c0fe708a4021a7cb5c14f0f0bc34f78
UoK424 <42653404+UoK424@users.noreply.github.com>
2019-01-17 15:06:13 +0000

    Update convertImage.py

--- Results/convertImage.py
+++ Results/convertImage.py
@@ -3,7 +3,7 @@ import itertools
 with open('Forensics.txt', 'r') as in_file:
     stripped = (line.strip() for line in in_file)
     lines = (line for line in stripped if line)
-    grouped = itertools.izip(*[lines] * 9)
+    grouped = zip(*[lines] * 9)
     with open('ForensicReport.csv', 'w') as out_file:
             writer = csv.writer(out_file)
             writer.writerow(('File Name', 'File Size', 'Image Size', 'File Type Extension', 'File Access Date', 'File Modify Date', 'SHA3 512 Hash', 'Date', 'UID'))
