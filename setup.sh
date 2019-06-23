e90f69b51e81e2834dff9ffd401a6b5c98dd3c0a
DarrenHurleySmith <d.hurleysmith@gmail.com>
2019-06-23 12:26:50 -0700

    setup fixed

--- setup.sh
+++ setup.sh
@@ -5,6 +5,10 @@ cd ..
 
 sudo -H pip3 install -r RAMSES_StegAware/requirements.txt
 sudo apt install libimage-exiftool-perl mp4v2-utils openjdk-11-jre-headless libdigest-sha3-perl
+
+sudo -H pip3 install -r RAMSES_StegAware/requirements.txt
+sudo apt install libimage-exiftool-perl mp4v2-utils openjdk-11-jre-headless libdigest-sha3-perl
+
 sudo chmod 755 -R RAMSES_StegAware/
 
 cd RAMSES_StegAware/
