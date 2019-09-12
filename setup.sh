ee70628c890d18f102007e64c6ab0fef9bdfb619
darren <d.hurleysmith@gmail.com>
2019-09-12 14:59:48 +0200

    winning the war against bad requirements files

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ cd ..
 sudo apt install python3-pip
 sudo apt install libimage-exiftool-perl mp4v2-utils openjdk-11-jre-headless libdigest-sha3-perl
 sudo -H pip3 install -r RAMSES_StegAware/requirements.txt
+#sudo pip3 install PyQt5
 
 #sudo -H pip3 install -r RAMSES_StegAware/requirements.txt
 #sudo apt install libimage-exiftool-perl mp4v2-utils openjdk-11-jre-headless libdigest-sha3-perl
